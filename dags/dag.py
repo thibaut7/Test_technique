32ce95775645e09b1ea4d93a6ae2efd065ea6585
thibaut7 <thibautmcdonald@gmail.com>
2025-03-15 16:05:18 +0000

    readme

--- dags/dag.py
+++ dags/dag.py
@@ -6,11 +6,11 @@ import json
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2025, 3, 15),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retries': 2,
+    'retry_delay': timedelta(minutes=1),
 }
 
-with DAG('ssh_postgres_test', default_args=default_args, schedule = '@minute' ) as dag:
+with DAG('ssh_postgres_test', default_args=default_args, schedule = '@hourly' ) as dag:
     command = '''
         SSH_KEY="{{ json.load(open('/opt/airflow/config/params.json'))['ssh_key'] }}"
         PASSPHRASE="{{ json.load(open('/opt/airflow/config/params.json'))['passphrase'] }}"
